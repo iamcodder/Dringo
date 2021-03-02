32333444288ed84c3b2f8f0e85b478bb79a26200
Süleyman Sezer <me.iamcodder@gmail.com>
2021-03-02 06:17:20 +0300

    Language card ui added (not clickable)

--- app/src/main/java/com/iamcodder/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/iamcodder/myapplication/ui/theme/Color.kt
@@ -8,3 +8,9 @@ val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 
 val PurpleBackground = Color(0xFF7114BD)
+
+val Grey800 = Color(0xFF585757)
+val Grey700 = Color(0xFF707070)
+val Grey600 = Color(0xFF9E9898)
+
+val Green500 = Color(0xFFFFEB3B)
